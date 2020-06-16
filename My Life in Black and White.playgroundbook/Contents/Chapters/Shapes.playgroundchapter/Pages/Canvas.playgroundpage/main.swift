15e2d230f5b8bb7a629185358d0b6ac23cd80b4b
Eliza Carvalho <elizampcarvalho@gmail.com>
2020-06-16 14:06:33 -0300

    Project WWDC2020

---
+++ My Life in Black and White.playgroundbook/Contents/Chapters/Shapes.playgroundchapter/Pages/Canvas.playgroundpage/main.swift
@@ -0,0 +1,17 @@
+//#-hidden-code
+//
+//  main.swift
+//  
+//  Copyright © 2016-2019 Apple Inc. All rights reserved.
+//
+//#-end-hidden-code
+//#-code-completion(module, hide, Swift)
+//#-code-completion(identifier, hide, AbstractDrawable, _ColorLiteralType, _setup())
+//#-hidden-code
+_setup()
+//#-end-hidden-code
+//#-editable-code Tap to enter code
+let circle = Circle()
+circle.draggable = true
+
+//#-end-editable-code
